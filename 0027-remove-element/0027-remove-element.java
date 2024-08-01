ec6ac1dbc744a3b7dc0abea76fc68def5126b0ec
sunheeroom <150791882+sunheeroom@users.noreply.github.com>
2024-08-01 10:10:27 +0900

    Time: 0 ms (100%), Space: 41.6 MB (88.93%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,20 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int result = 0;
-        Queue<Integer> vacant = new LinkedList<>();    // 제거한 위치
-        int count = 0;
+        int k=0;
 
         for(int i=0; i < nums.length; i++) {
-            if ( nums[i] == val ) {
-                vacant.add(i);
-            } else {
-                count++;
-                if (vacant.size() > 0) {
-                    nums[vacant.poll()] = nums[i];
-                    vacant.add(i);
-                }
+            if ( nums[i] != val ) {
+                nums[k++] = nums[i]; 
             }
         }
-        return count;
+        return k;
     }
 }
